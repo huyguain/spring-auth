49af59aff0953504e35e2ec1296679eb1dd7739b
huy.nguyen21 <huy.nguyen21@onemount.com>
2024-11-21 11:44:07 +0700

    i18n, register controller-dto

---
+++ src/main/java/com/example/spring_auth/controllers/UserController.java
@@ -0,0 +1,65 @@
+package com.example.spring_auth.controllers;
+
+import com.example.spring_auth.components.LocalizationUtils;
+import com.example.spring_auth.dtos.UserDTO;
+import com.example.spring_auth.responses.ResponseObject;
+import com.example.spring_auth.utils.MessageKeys;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final LocalizationUtils localizationUtils;
+    @GetMapping("/login")
+    public String login() throws Exception {
+        try {
+            return "Success!";
+        } catch (Exception e) {
+            return "Error!";
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseObject> register(
+            @Valid @RequestBody UserDTO userDTO,
+            BindingResult result
+    ) throws Exception {
+        if (result.hasErrors()) {
+            List<String> errorMessage = result.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .toList();
+            return ResponseEntity.badRequest().body(
+                    ResponseObject.builder()
+                            .message(errorMessage.toString())
+                            .status(HttpStatus.BAD_REQUEST)
+                            .data(null)
+                            .build()
+            );
+        }
+        if (!userDTO.getPassword().equals(userDTO.getRetypePassword())) {
+            return ResponseEntity.badRequest().body(
+                    ResponseObject.builder()
+                            .message(localizationUtils.getLocalizedMessage(MessageKeys.PASSWORD_NOT_MATCH))
+                            .status(HttpStatus.BAD_REQUEST)
+                            .data(null)
+                            .build()
+            );
+        }
+        return ResponseEntity.ok(
+                ResponseObject.builder()
+                        .message("User registered successfully")
+                        .status(HttpStatus.OK)
+                        .data(userDTO)
+                        .build()
+        );
+    }
+}
