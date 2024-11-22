23ec7b24e4d48f160eb6bd10b2f5d87b70d37e03
huy.nguyen21 <huy.nguyen21@onemount.com>
2024-11-22 16:21:29 +0700

    register: model BaseEntity, Exceptions handle, SecurityConfig

---
+++ src/main/java/com/example/spring_auth/exceptions/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.spring_auth.exceptions;
+
+public class DataNotFoundException extends Exception{
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
